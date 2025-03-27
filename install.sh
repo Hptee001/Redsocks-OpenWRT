ba1b45cb632f4cce822d102f321c3e942b9502f0
Hptee001 <hptee001@gmail.com>
2025-03-27 15:18:21 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -10,3 +10,5 @@ opkg install iptables iptables-mod-nat-extra redsocks
 service redsocks stop && mv /etc/redsocks.conf /etc/redsocks.conf.bkp && cd /etc && wget https://github.com/hptee001/Redsocks-OpenWRT/raw/main/redsocks.conf && mv /etc/init.d/redsocks /etc/init.d/redsocks.bkp && cd /etc/init.d && wget https://github.com/hptee001/Redsocks-OpenWRT/raw/main/redsocks && chmod +x /etc/init.d/redsocks
 
 echo -e "Thanks for installing. Follow me for more updates. https://fb.me/emoncontact"
+
+service redsocks start
